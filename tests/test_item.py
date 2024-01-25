19ed9ebca3ae0017ca0a2c5c64904d22985c8da9
fenia3004 <fenia3004@gmail.com>
2024-01-25 22:34:25 +0300

    Написание кода

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,13 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+item = Item('name', 200.0, 2)
+pay_rate = 0.85
+
+def test_calculate_total_price():
+    assert item.calculate_total_price() == 400
+    assert type(item.calculate_total_price()) == float
+
+
+def test_apply_discount():
+    assert item.price * pay_rate == 170
