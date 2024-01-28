9abace0c1527102962f1ee414ac772f8ba4be2c5
fenia3004 <fenia3004@gmail.com>
2024-01-28 20:19:56 +0300

    Выполнение ДЗ

--- src/item.py
+++ src/item.py
@@ -1,3 +1,7 @@
+import csv
+from pathlib import Path
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,7 +17,7 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         Item.all.append(self)
@@ -33,3 +37,41 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price *= self.pay_rate
+
+    @property
+    def name(self):
+        """
+        Геттер для приватного атрибута name.
+        Возвращает значение приватного атрибута name.
+
+        :return: Название товара.
+        """
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        """
+        Сеттер для приватного атрибута name.
+        Присваивает значение приватному атрибуту name.
+        Если длина переданного значения больше 10 символов, обрезает его до 10 символов.
+
+        :param value: Новое значение для названия товара.
+        """
+        name = value[:10]
+        self.__name = name
+
+    @classmethod
+    def instantiate_from_csv(cls, way):
+        cls.all.clear()
+        with open(way, newline='') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                name = row['name']
+                price = cls.string_to_number(row['price'])
+                quantity = int(row['quantity'])
+                cls(name, price, quantity)
+
+    @staticmethod
+    def string_to_number(value: str) -> float:
+        """Преобразует строку с числом в число."""
+        return int(float(value))
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,8 +1,9 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
 from src.item import Item
 
-item = Item('name', 200.0, 2)
+item = Item('namenamename', 200.0, 2)
 pay_rate = 0.85
+
 
 def test_calculate_total_price():
     assert item.calculate_total_price() == 400
@@ -11,3 +12,13 @@ def test_calculate_total_price():
 
 def test_apply_discount():
     assert item.price * pay_rate == 170
+
+# def test_name():
+#     assert item.name == 'name'
+
+# def test_name_setter():
+#     assert len(Item.name) <=  10
+
+
+def test_string_to_number():
+    assert type(Item.string_to_number(item.price)) == int
