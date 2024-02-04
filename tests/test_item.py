a845b00563402ef2869d58498e3b3c1a8944908c
fenia3004 <fenia3004@gmail.com>
2024-02-04 13:38:12 +0300

    Выполнение ДЗ 14.1

--- tests/test_item.py
+++ tests/test_item.py
@@ -4,6 +4,14 @@ from src.item import Item
 item = Item('namenamename', 200.0, 2)
 pay_rate = 0.85
 
+short_name = "короткое"
+long_name = "длинноеимяяяя"
+
+item.name = short_name
+assert item.name == short_name
+
+item.name = long_name
+assert item.name == long_name[:10]
 
 def test_calculate_total_price():
     assert item.calculate_total_price() == 400
@@ -13,12 +21,13 @@ def test_calculate_total_price():
 def test_apply_discount():
     assert item.price * pay_rate == 170
 
-# def test_name():
-#     assert item.name == 'name'
-
-# def test_name_setter():
-#     assert len(Item.name) <=  10
-
 
 def test_string_to_number():
     assert type(Item.string_to_number(item.price)) == int
+
+
+def test__repr__():
+    assert repr(item) == "Item('длинноеимя', 200.0, 2)"
+
+def test__str__():
+    assert str(item) == 'длинноеимя'
